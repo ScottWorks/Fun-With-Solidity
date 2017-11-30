c2bc4f6325c9efcad64fa22260160a6130bf5527
ScottWorks <23440493+ScottWorks@users.noreply.github.com>
2017-11-29 23:43:33 -0500

    completed all modules

--- Coursetro/Coursetro.sol
+++ Coursetro/Coursetro.sol
@@ -1,33 +1,56 @@
 pragma solidity ^0.4.18;
 
-contract Coursetro {
-    
-   string fName;
-   uint age;
-   address owner; 
-   
-    event Instructor (
-        string name,
-        uint age
-    );
+contract Owned {
+    address owner;
 
-    function Coursetro () public {
-        owner = msg.sender; 
+    function Owned() public {
+        owner = msg.sender;
     }
 
     modifier onlyOwner {
-        require (msg.sender == owner); // if 'msg.sender' does not equal 'owner', throw 
-        _; // else execute
+        require(msg.sender == owner);
+        _;
+    }
+}
+
+contract Coursetro is Owned {
+
+    struct Instructor {
+        bytes16 fName;
+        bytes16 lName;
+        uint age;
+    }
+   
+    mapping (address => Instructor) instructors;
+    address[] public instructorAccts; 
+
+    event InstructorInfo(
+        bytes16 fName,
+        bytes16 lName,
+        uint age
+    );
+
+    function setInstructor (address _address, uint _age, bytes16 _fName, bytes16 _lName) onlyOwner public {
+        var instructor = instructors[_address];
+
+        instructor.age = _age;
+        instructor.fName = _fName;
+        instructor.lName = _lName;
+
+        instructorAccts.push(_address) - 1; 
+        InstructorInfo(_fName, _lName, _age);
     }
 
-    function setInstructor(string _fName, uint _age) onlyOwner public {
-       fName = _fName;
-       age = _age;
-       Instructor (_fName, _age);
-   }
-   
-    function getInstructor() public constant returns (string, uint) {
-       return (fName, age);
-   }
-    
+    function getInstructors () view public returns (address[]) {
+        return instructorAccts; 
+    }
+
+    function getInstructor (address _address) view public returns (uint, bytes16, bytes16) {
+        return (instructors[_address].age, instructors[_address].fName, instructors[_address].lName);
+    }
+
+    function countInstructors () view public returns (uint) {
+        return instructorAccts.length;
+    }
 }
+
